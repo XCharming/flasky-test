911249a0917b95573521590eb87ed2ee0fa3ce71
XCharming <xmingx@outlook.com>
2018-01-04 20:03:34 +0800

    first_commit

---
+++ app/__init__.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_required
+from flask_pagedown import PageDown
+from config import config
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+pagedown = PageDown()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    pagedown.init_app(app)
+
+    #附加路由和自定义的错误界面
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    #附加的验证登录
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    #注册API蓝本
+    from .api_1_0 import api as api_1_0_blueprint
+    app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
+
+    @app.route('/secret')
+    @login_required
+    def secret():
+        return 'Only authenticated users are allowed!'
+    
+    return app
